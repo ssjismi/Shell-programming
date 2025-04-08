51d21ce45ae2ea8b76704167aeb2a84c3ce61505
ssjismi <160107907+ssjismi@users.noreply.github.com>
2025-04-08 19:55:36 +0530

    Create vowels1.sh

---
+++ vowels1.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+read -p "Enter a string" str
+str=$(echo "$str" | tr '[:upper:]' '[:lower:]')
+vowe=0
+co=0
+for(( i=0;i<${#str};i++ )); do
+char="${str:i:1}"
+if [[ "$char" == [aeiou] ]]; then
+((vowe++))
+elif [[ "$char" =~ [a-z] ]]; then
+((co++))
+fi
+done
+echo "The no of vowels:$vowe"
+echo "the no of consonants is:$co"
+
